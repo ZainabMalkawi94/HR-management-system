3dad5264e23777be5048df8c286faed02b49d96a
ZainabMalkawi94 <zainab.m.malkawi@gmail.com>
2023-04-12 11:35:27 +0000

    make Feedback changes

--- JavaScript/App.js
+++ JavaScript/App.js
@@ -48,7 +48,7 @@ EmployeeInfo.prototype.EmployeeID=function()
 }
 
 EmployeeInfo.prototype.Render =function()
-{
+{  // const div =;
    const imgEl = document.createElement('img');
    imgEl.src = this.imgURL;
    infoSection.appendChild(imgEl);
@@ -63,33 +63,33 @@ EmployeeInfo.prototype.Render =function()
    infoSection.appendChild(IDEl);
 
    const departmentEl = document.createElement('p');
-   if (this.department == '1') {
+   if (this.department == '1'|| this.department == "Administration" ) {
       departmentEl.textContent = `Department: Administration`;
       infoSection.appendChild(departmentEl);}
 
-   else if (this.department == '2') {
+   else if (this.department == '2'|| this.department == "Marketing" ) {
       departmentEl.textContent = `Department: Marketing`;
       infoSection.appendChild(departmentEl);}
 
-   else if (this.department == '3') {
+   else if (this.department == '3'|| this.department == "Development") {
       departmentEl.textContent = `Department: Development`;
       infoSection.appendChild(departmentEl);}
 
-   else if (this.department == '4') {
-      departmentEl.textContent = `Department: Finance`;
+   else if (this.department == '4'|| this.department == "Fainance") {
+      departmentEl.textContent = `Department: Fainance`;
       infoSection.appendChild(departmentEl);}
 
 
    const levelEl = document.createElement('p');
-   if(this.level == '1')
+   if(this.level == '1' || this.level == "Junior")
    {levelEl.textContent = `Level: Junior`;
    infoSection.appendChild(levelEl);}
 
-   else if(this.level == '2')
+   else if(this.level == '2'|| this.level == "Mid-Senior")
    {levelEl.textContent = `Level: Mid-Senior`;
    infoSection.appendChild(levelEl);}
 
-   else if(this.level == '3')
+   else if(this.level == '3'|| this.level == "Senior")
    {levelEl.textContent = `Level: Senior`;
    infoSection.appendChild(levelEl);}
 
@@ -105,7 +105,7 @@ EmployeeInfo.prototype.Salary = function (level) {
     let max = 0;
     let salary = 0;
     let randomSalary = 0;
-    if (level == 1) {
+    if (level == 1 || level == "Junior" ) {
         min = 500;
         max = 1000;
         randomSalary = Math.floor(Math.random() * (max - min + 1)) + min;
@@ -113,13 +113,13 @@ EmployeeInfo.prototype.Salary = function (level) {
 
     }
 
-    else if (level == 2) {
+    else if (level == 2|| level == "Mid-Senior" ) {
         min = 1000;
         max = 1500;
         randomSalary = Math.floor(Math.random() * (max - min + 1)) + min;
         this.salary = randomSalary-randomSalary * tax;
     }
-    else if (level == 3) {
+    else if (level == 3|| level == "Senior" ) {
         min = 1500;
         max = 2000;
         randomSalary = Math.floor(Math.random() * (max - min + 1)) + min;
@@ -130,8 +130,19 @@ EmployeeInfo.prototype.Salary = function (level) {
     console.log(this.salary);
 }
 /*************************************************************************************************************************************/
+
+let ghaziSamer = new EmployeeInfo('Ghazi Samer', 'Administration', 'Senior','./assets/pic1.png');
+let lanaAli = new EmployeeInfo('Lana Ali', 'Fainance', 'Senior','./assets/pic1.png');
+let tamaraAyoub = new EmployeeInfo('Tamara Ayoub', 'Marketing', 'Senior','./assets/pic1.png');
+let saifWaleed = new EmployeeInfo('Safi Walid', 'Administration', 'Mid-Senior','./assets/pic1.png');
+let omarZaid = new EmployeeInfo('Omar Zaid', 'Development', 'Senior' , './assets/pic1.png');
+let ranaSaleh = new EmployeeInfo('Rana Saleh', 'Development', 'Junior', './assets/pic1.png');
+let hadiAhmad = new EmployeeInfo('Hadi Ahmad', 'Fainance', 'Mid-Senior', './assets/pic1.png');
+
+console.log (employeeArray);
 for(let i = 0 ; i < employeeArray.length ; i++){
-    employeeArray[i].salary(employeeArray[i].level);
-    employeeArray[i].render()
+    employeeArray[i].EmployeeID();
+    console.log (employeeArray[i].level);
+    employeeArray[i].Salary(employeeArray[i].level);
+    employeeArray[i].Render();
 }
-console.log(employeeArray);
